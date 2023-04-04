0d285c075ffc41f795c2f758dad07875ef2748a7
Moaz El-sawaf <moazelsawaf@hotmail.com>
2023-04-04 23:42:30 +0200

    feat: :sparkles: subscribe the user automatically to "all" fcm topic after the auth

--- lib/screens/loadingHome.dart
+++ lib/screens/loadingHome.dart
@@ -3,11 +3,13 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dr_nashar/user/yearsData.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:rive/rive.dart';
 
 import '../user/UserID.dart';
+import '../utils/constants.dart';
 
 class LoadingHomeScreen extends StatefulWidget {
   const LoadingHomeScreen({Key? key}) : super(key: key);
@@ -22,6 +24,8 @@ class _LoadingHomeScreenState extends State<LoadingHomeScreen> {
   @override
   void initState() {
     super.initState();
+    FirebaseMessaging.instance.subscribeToTopic(Constants.fcmAllTopic);
+
     loadData();
   }
 
